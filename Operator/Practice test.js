7c962996c796c9a1891a4dc939bd7c5c469a2e2f
JyotiPrakashPanda <pandajyotiprakash33@gmail.com>
2022-09-05 14:40:04 +0530

    practice 1

---
+++ Operator/Practice test.js
@@ -0,0 +1,16 @@
+//practice 1
+var a= 5
+var b = 10
+add = a+b
+sub = b-a
+mul = a*b
+div = a/b
+inc = ++a
+dec = --b
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(inc);
+console.log(dec);
